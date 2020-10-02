96ed5e32360f4da642caa4aed0fde0b0da33fb32
Himanshi Tiwari <tiwari.himanshi09@gmail.com>
2020-10-02 21:06:24 +0530

    second commit

--- app.js
+++ app.js
@@ -192,6 +192,6 @@ app.post("/login", function(req, res) {
   });
 });
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Server started on port 3000");
 });
